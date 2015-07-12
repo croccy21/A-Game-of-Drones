354abf06e73c3da045c993523ea46ddc432b0874
croccy21 <croccy21@users.noreply.github.com>
2015-07-12 20:40:14 +0100

    Added radio tower & day-night cycle
    Radio tower currently doesn't do anything and day-night cycle speed is
    increased for testing purposes.

---
+++ Extended_Project_Game/Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayNightCycle : MonoBehaviour {
+
+	private Transform transform;
+	public float timeConstant = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		transform = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Rotate (new Vector3 (Time.deltaTime*timeConstant, 0 , 0));
+	}
+}
