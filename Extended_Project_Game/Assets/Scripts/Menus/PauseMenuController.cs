d358bcb59773f3419ce9c84a79168fe4a9501bb5
croccy21 <croccy21@users.noreply.github.com>
2015-07-29 22:11:56 +0100

    Pause on escape
    Has expandable menu system that allows for new menus to be added. Resume
    and Quit buttons should work (although quit is untested).

---
+++ Extended_Project_Game/Assets/Scripts/Menus/PauseMenuController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenuController : MonoBehaviour {
+
+	public GameObject[] menu;
+	private GameObject currentMenu;
+	private int currentID = -1;
+	private Canvas globalCanvas;
+
+	private bool pauseButtonPressed = false;
+
+	// Use this for initialization
+	void Start () {
+		globalCanvas = GetComponent<Canvas> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxisRaw ("pause") == 1) {
+			if (!pauseButtonPressed) {
+				if (currentID < 0) {
+					setMenu (0);
+				} else {
+					setMenu (-1);
+				}
+				pauseButtonPressed = true;
+			}
+		} else if (pauseButtonPressed) {
+			pauseButtonPressed = false;
+		}
+	}
+
+	public void setMenu(int menuID){
+		if (menuID < menu.Length) {
+			currentID = menuID;
+			if (menuID >= 0) {
+				if (!globalCanvas.enabled) {
+					globalCanvas.enabled = true;
+					Time.timeScale=0;
+				}
+				if (currentMenu != null) {
+					currentMenu.SetActive (false);
+				}
+				menu [menuID].SetActive (true);
+				currentMenu = menu [menuID];
+			} else if (menuID == -1) {
+				globalCanvas.enabled = false;
+				if (currentMenu != null) {
+					currentMenu.SetActive (false);
+				}
+				Time.timeScale=1;
+				currentMenu = null;
+			}
+		}
+	}
+}
